c00d520313cec0db0cc4c0e3c5eb96854d12c725
john benjamin <johnzeus14@gmail.com>
2018-12-26 21:28:27 +0100

    removed icons folder

--- src/api/backend/story.js
+++ src/api/backend/story.js
@@ -6,26 +6,18 @@ import axios from 'axios'
 export default {
 
 
-axios.get(url, request) {
- return Vue.axios.get(url, request)
- .then((response) => Promise.resolve(response.body.data))
- .catch((error) => Promise.reject(error));
- },
- axios.post(url, request) {
- return Vue.axios.post(url, request)
- .then((response) => Promise.resolve(response))
- .catch((error) => Promise.reject(error));
- },
- axios.patch(url, request) {
- return Vue.axios.patch(url, request)
- .then((response) => Promise.resolve(response))
- .catch((error) => Promise.reject(error));
- },
- axios.delete(url, request) {
- return Vue.axios.delete(url, request)
- .then((response) => Promise.resolve(response))
- .catch((error) => Promise.reject(error));
- }
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 
 	
 }
